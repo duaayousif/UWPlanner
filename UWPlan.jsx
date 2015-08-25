3e35e3b29807b340d765c81782e54a7216be232d
duaayousif <dyaabdel@uwaterloo.ca>
2015-08-26 03:15:49 +0400

    Added TODOs

--- UWPlan.jsx
+++ UWPlan.jsx
@@ -1,18 +1,5 @@
 var FacultyDropdown = React.createClass({
-    getInitialState: function(){
-       return{
-         chosenfaculty: "Major"
-
-       }
-    },
-    handleFaculty: function(faculty) {
-        this.setState({chosenfaculty: faculty})
-    },
     render: function() {
-
-        var faculties = ["Applied Health        Science","Arts","Engineering","Environment","Mathematics","Science"];
-        debugger;
-
         return(
           <div className="dropdown">
           <h4> {this.props.dropdownText} </h4>
@@ -23,59 +10,68 @@ var FacultyDropdown = React.createClass({
          <ul className="dropdown-menu" aria-labelledby="dLabel">
 
            debugger;
-            {faculties.map(function(faculty){return <li onClick = {this.handleFaculty(faculty)}><a href="#">{faculty}</a></li>}).bind(this)}
-
             {this.props.dropDownMenu.map(function(faculty){
                return <li onClick={this.props.whenClicked.bind(this,faculty)}><a href="#">{faculty}</a></li>}.bind(this))}
           </ul>
           </div>
         )}
      ,
-
-
+    })
 
 var CreatePageApp = React.createClass({
   getInitialState: function(){
     return{
       data: "",
       chosenfaculty: "Faculty",
+      chosenprogram: "Program",
       chosenProgArray: [],
-      facultyArray: ["Applied Health  Science","Arts","Engineering","Environment","Mathematics","Science"]
+      facultyArray: ["Applied Health Sciences","Arts","Engineering","Environment","Mathematics","Science"]
     }
   },
 
   handleFaculty: function(faculty) {
         this.setState({chosenfaculty: faculty})
-        this.chosenArray();
+        this.chosenArray(faculty);
     },
+  handleProgram: function(program) {
+      this.setState({chosenprogram: program});
+  },
+  chosenProgram: function() {
+    return(
+      this.state.chosenprogram
+    )
+  },
 
     chosenFaculty: function(){
         return(
             this.state.chosenfaculty
         )
     },
+    
+    //TODO: Get the first case working for alert box
+    //TODO: Get rid of thhe debugger thing ?? 
 
-    chosenArray: function(){
-        if (this.state.chosenfaculty === "Major") {
+    chosenArray: function(faculty){
+        if (faculty === "Faculty") {
             alert("Choose your faculty first");
         }
-        else if (this.state.chosenfaculty === "Applied Health Sciences") {
-            this.setState({chosenProgArray: ["fdsf","fdsf"]});
+        else if (faculty === "Applied Health Sciences") {
+            this.setState({chosenProgArray: ["Health Studies", "Kinesiology","Public Health","Recreation and Leisure Studies","Recreation and Sport Business", "Therapeutic Recreation","Tourism Development"]});
         }
-        else if (this.state.chosenfaculty === "Arts") {
-            this.setState({chosenProgArray: ["fds","fd"]});
+        else if (faculty === "Arts") {
+            this.setState({chosenProgArray: ["Accounting and Financial Management","Anthropology","Classical Studies","Computing and Financial Management","Economics","English","Fine Arts","French","German","Global Business and Digital Arts","History","Honours Arts","Honours Arts and Business","Independent Studies","Legal Stuies","Liberal Studies","Medieval Studies","Music","Peace and Conflict Studies","Philosophy","Political Science","Psychology","Religious Studies","Russian and East European Studies","Sexuality, Marriage, and Family Studies","Social Development Studies","Sociology","Spanish","Speech Communication","Theatre and Performance","Women's Studies"]});
         }
-        else if (this.chosenfaculty === "Engineering") {
-            this.setState({chosenProgArray: ["f"]});
+        else if (faculty === "Engineering") {
+            this.setState({chosenProgArray: ["Architecture","Biomedical Engineering","Chemical Engineering","Civil Engineering","Computer Engineering","Electrical Engineering","Environmental Engineering","Geological Engineering","Management Engineering","Mechatronics Engineering","Nanotechnology Engineering","Software Engineering","Systems Design Engineering"]});
         }
-        else if (this.chosenfaculty === "Environment") {
-            this.setState({chosenProgArray: ["e"]})
+        else if (faculty === "Environment") {
+            this.setState({chosenProgArray: ["Environment and Business","Environment and Resource Studies","Geography and Aviation","Geography and Environmental Management","Geomatics","International Development","Knowledge Integration","Planning"]})
         }
-        else if (this.chosenfaculty === "Mathematics") {
-            this.setState({chosenProgArray: ["fs"]})
+        else if (faculty === "Mathematics") {
+            this.setState({chosenProgArray: ["Actuarial Science", "Applied Mathematics","Business Administration and Computer Science Double Degree","Business Administration and Mathematics Double Degree", "Combinatorics and Optimization","Computational Mathematics","Computer Science","Computing and Financial Management","Information Technology Management","Mathematical Economics","Mathematical Finance","Mathematical Optimization","Mathematical Physics","Mathematics","Mathematics/Business Administration","Mathematics/Chartered Professional Accounting","Mathematics/Financial Analysis and Risk Management","Mathematics Teaching","Pure Mathematics","Software Engineering","Statistics","Statistics for Health"]})
         }
-        else if (this.chosenfaculty === "Science") {
-            this.setState({chosenProgArray: ["ew"]})
+        else if (faculty === "Science") {
+            this.setState({chosenProgArray: ["Biochemistry","Biology","Biomedical Sciences","Biotechnology/Chartered Professional Accounting","Biotechnology/Economics","Chemistry","Earth Sciences","Environmental Science","Honours Science","Life Physics","Life Sciences","Materials and Nanosciences","Mathematical Physics","Medicinal Chemistry","Physical Sciences","Physics","Physics and Astronomy","Psychology","Science and Aviation","Science and Business"]})
         }
     },
 
@@ -99,7 +95,7 @@ var CreatePageApp = React.createClass({
         <FacultyDropdown dropdownText="Select your faculty" whenClicked={this.handleFaculty} faculty={this.chosenFaculty()} dropDownMenu = {this.state.facultyArray}/>
         </div>
         <div>
-        <FacultyDropdown dropdownText = "Select your program" whenClicked={this.handleFaculty} faculty={this.chosenFaculty()} dropDownMenu = {this.state.chosenProgArray} />
+        <FacultyDropdown dropdownText = "Select your program" whenClicked={this.handleProgram} faculty={this.chosenProgram()} dropDownMenu = {this.state.chosenProgArray} />
         </div>
 </div>
       )
