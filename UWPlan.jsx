abd0740066920a2adf5c7284fd197a82378cabc7
duaayousif <dyaabdel@uwaterloo.ca>
2015-08-25 17:20:51 +0400

    handleFact

--- UWPlan.jsx
+++ UWPlan.jsx
@@ -1,3 +1,30 @@
+var FacultyDropdown = React.createClass({
+    getInitialState: function(){
+       return{    
+         chosenfaculty: "Major"
+       }
+    },
+    handleFaculty: function(faculty) {
+        this.setState({chosenfaculty: faculty})
+    },
+    render: function() {
+        var faculties = ["Applied Health        Science","Arts","Engineering","Environment","Mathematics","Science"];
+        return(
+          <div className="dropdown">
+          <h4> {this.props.dropdownText} </h4>
+          <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {this.state.chosenfaculty}
+          <span className="caret"></span>
+          </button>
+         <ul className="dropdown-menu" aria-labelledby="dLabel">
+            {faculties.map(function(faculty){return <li onClick = {this.handleFaculty.bind(this,faculty)}><a href="#">{faculty}</a></li>})}
+          </ul>
+          </div>
+        )}
+     ,
+})
+    
+
 
 var CreatePageApp = React.createClass({
   getInitialState: function(){
@@ -15,66 +42,17 @@ var CreatePageApp = React.createClass({
   debugger;
   console.log(xhr.responseText);
 },
+    handleFactDrop: function(event) {
+        this.setState({ facultyDrop: event.target.value });
+    },
 
     render: function(){
       return(
-          
-        <div className="container containerwidth">
+        <div className="container-fluid containerwidth">
           <div className="headerback">
              <h2> UW Plan </h2>
           </div>
-            <div class="dropdown">
-                  <h3> Select your Major </h3>
-                 <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"    aria-haspopup="true" aria-expanded="true">
-                 Dropdown
-                 <span class="caret"></span>
-                 </button>
-               <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                 <li class="dropdown-header">Faculty of Applied Health Science</li>
-                 <li><a href="#">Health Promotion</a></li>
-                 <li><a href="#">Health Studies</a></li>
-                 <li><a href="#">Kinesiology</a></li>
-                 <li><a href="#">Recreation and Leisure Studies</a></li>
-                 <li><a href="#">Recreation and Sport Business</a></li>
-                 <li><a href="#">Therapeutic Recreation</a></li>
-                 <li><a href="#">Kinesiology</a></li>
-                 <li><a href="#">Tourism and Parks Management</a></li>
-                 <li class="dropdown-header">Faculty of Arts</li>
-                 <li><a href="#">Accounting and Financial Management</a></li>
-                 <li><a href="#">Anthropology</a></li>
-                 <li><a href="#">Classical Studies</a></li>
-                 // computing and financial management
-                 <li><a href="#">Drama</a></li>
-                 <li><a href="#">Economics</a></li>
-                 <li><a href="#">English</a></li>
-                 <li><a href="#">Fine Arts</a></li>
-                 <li><a href="#">French</a></li>
-                 <li><a href="#">German</a></li>
-                 <li><a href="#">Global Business and Digital Arts</a></li>
-                 <li><a href="#">History</a></li>
-                 <li><a href="#">Honours Arts</a></li>
-                 <li><a href="#">Honours Arts and Business</a></li>
-                 <li><a href="#">Independent Studies</a></li>
-                 <li><a href="#">Legal Studies</a></li>
-                 <li><a href="#">Liberal Studies</a></li>
-                 <li><a href="#">Medieval Studies</a></li>
-                 <li><a href="#">Music</a></li>
-                 <li><a href="#">Peace and Conflict Studies</a></li>
-                 <li><a href="#">Philosophy</a></li>
-                 <li><a href="#">Political Science</a></li>
-                 <li><a href="#">Psychology</a></li>
-                 <li><a href="#">Religious Studies</a></li>
-                 <li><a href="#">Russian and East European Studies</a></li>
-                 <li><a href="#">Sexuality, Marriage, and Family Studies</a></li>
-                 <li><a href="#">Social Development Studies</a></li>
-                 <li><a href="#">Sociology</a></li>
-                 <li><a href="#">Spanish</a></li>
-                 <li><a href="#">Speech Communication</a></li>
-                 <li><a href="#">Women's Studies</a></li>
-                 <li><a href="#">Kinesiology</a></li>
-                 
-               </ul>
-            </div> 
+        <FacultyDropdown dropdownText="Choose your faculty"/>
         </div>
 
       )
